babb14c67208fa0d4c856b40ba513dafb7023dce
Billy Priambodo <edo.billy@gmail.com>
2025-06-26 14:06:05 +0700

    feat: tasks models

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,27 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class TaskStatus(models.TextChoices):
+    TODO = "TODO", "Todo"
+    IN_PROGRESS = "IN_PROGRESS", "In Progress"
+    DONE = "DONE", "Done"
+
+
+class Tasks(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20, choices=TaskStatus.choices, default=TaskStatus.TODO
+    )
+    assigned_to = models.ForeignKey(
+        "users.CustomUser", on_delete=models.SET_NULL, null=True, default=None
+    )
+    project = models.ForeignKey(
+        "projects.Projects", on_delete=models.CASCADE, related_name="tasks"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
