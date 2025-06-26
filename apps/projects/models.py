971c2d4bc1facb25dd38b2ba546cf1a0f2c58bb9
Billy Priambodo <edo.billy@gmail.com>
2025-06-26 15:28:10 +0700

    feat: seed group and permissions
    - fix rename projects and tasks model

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 
-class Projects(models.Model):
+class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -10,7 +10,7 @@ class TaskStatus(models.TextChoices):
     DONE = "DONE", "Done"
 
 
-class Tasks(models.Model):
+class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
@@ -21,7 +21,7 @@ class Tasks(models.Model):
         "users.CustomUser", on_delete=models.SET_NULL, null=True, default=None
     )
     project = models.ForeignKey(
-        "projects.Projects", on_delete=models.CASCADE, related_name="tasks"
+        "projects.Project", on_delete=models.CASCADE, related_name="tasks"
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
